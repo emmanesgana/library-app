b060840d94f0a0f6e536f9880e0fcc9168680e94
emmanesgana <emman.esgana@gmail.com>
2024-09-19 21:57:40 +0800

    Updated styles of modal and header.

---
+++ data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+        title: 'Atomic Habits',
+        author: 'James Clear',
+        pagesRead: 87,
+        hasRead: false
+    },
+    {
+        title: 'The Lord of the Rings',
+        author: 'J.R.R. Tolkien',
+        pagesRead: 178,
+        hasRead: true
+    },
+    {
+        title: 'Harry Potter',
+        author: 'J.K. Rowling',
+        pagesRead: 0,
+        hasRead: false
+    },
+    {
+        title: 'Game of Thrones',
+        author: 'George R.R. Martin',
+        pagesRead: 250,
+        hasRead: true
+    },
+
+]
+
+export default data
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import data from "./data.js"
+
 const modal = document.getElementById("modal");
 const modalBg = document.getElementById("modal-bg")
+const root = document.getElementById("root");
 const addBookButton = document.getElementById("add-book");
 const closeModalButton = document.getElementById("modal-close");
+
+function renderBooks(arr) {
+    return arr.forEach(item => {
+        return root.innerHTML += `
+            <div id="book" class="book">
+                <h3>${item.title}</h3>
+            </div>
+        `
+    })
+}
 
 addBookButton.addEventListener("click", () => {
     modal.style.display = "block";
@@ -12,3 +25,6 @@ closeModalButton.addEventListener("click", () => {
     modal.style.display = "none";
     modalBg.style.display = "none";
 });
+
+
+renderBooks(data)
